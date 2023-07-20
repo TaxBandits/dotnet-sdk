d707fb080225102c7b77139f4ae84710a6392ccc
sts-developer <139776689+sts-developer@users.noreply.github.com>
2023-07-20 15:20:04 +0530

    TaxBandits Public API - TIN Matching Recipient Endpoints Added

---
+++ tin-matching-recipients-sdk/TinMatchingRecipientsSDK/Models/Business/BusinessListResponse.cs
@@ -0,0 +1,15 @@
+﻿using System.Runtime.Serialization;
+using TinMatchingRecipientsSDK.Models.Base;
+
+namespace TinMatchingRecipientsSDK.Models.Business
+{
+    public class BusinessListResponse : BaseResponseStatus
+    {
+        /// <summary>
+        /// Business Details of all the business
+        /// </summary>
+        [DataMember(Order = 1)]
+        public List<Business> Businesses { get; set; }
+        public List<Error> Errors { get; set; }
+    }
+}
---
+++ tin-matching-recipients-sdk/TinMatchingRecipientsSDK/Models/TinMatchingRequest/TINMatchingBusiness.cs
@@ -0,0 +1,15 @@
+﻿using System.Runtime.Serialization;
+
+namespace TinMatchingRecipientsSDK.Models.TinMatchingRequest
+{
+    [DataContract]
+    public class TINMatchingBusiness
+    {
+        [DataMember]
+        public Guid? BusinessId { get; set; }
+        [DataMember]
+        public string BusinessNm { get; set; }
+        [DataMember]
+        public string TIN { get; set; }
+    }
+}
